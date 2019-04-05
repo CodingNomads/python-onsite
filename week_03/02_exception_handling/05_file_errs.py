d9dc459bb3c6f0447ef3f1c88b7c07522bf38e1d
Martin Breuss <martin-martin@users.noreply.github.com>
2019-04-05 15:52:27 +0800

    fix error type

--- week_03/02_exception_handling/05_file_errs.py
+++ week_03/02_exception_handling/05_file_errs.py
@@ -1,7 +1,7 @@
 '''
 Read in the first number from 'integers.txt' and perform a calculation
 with it.
-Make sure to catch at least two possible Exceptions (IOError and ValueError)
+Make sure to catch at least two possible Exceptions (FileNotFoundError and ValueError)
 with specific except statements, and continue to do the calculation
 only if neither of them applies.
 
